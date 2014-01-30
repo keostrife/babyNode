aa03ad26c21a6b4dc046ce37154ade0d5423604a
Keo Strife <keo_strife@yahoo.com>
2014-01-30 15:34:08 -0500

    minor details

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ __ROOTPATH__ = __dirname;
 
 //initiate
 require("./modules/babyNode");
-
 bN().init(8080).import("jQuery");
 
 
@@ -15,9 +14,12 @@ bN().init(8080).import("jQuery");
 
 IO.on("connection", function(socket){
 	socket.on("clientReady", function(){
+
+		//start coding!!!
 		$("body", socket).css({backgroundColor:"green"}).click(function(){
 			alert('heheh');
 		});
+		
 	});
 });
 
