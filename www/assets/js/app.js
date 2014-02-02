5b54e0442dd0e19b30281f5a33a104e03bcbbf7e
Keo Strife <keo_strife@yahoo.com>
2014-02-02 13:27:02 -0500

    update client security

--- www/assets/js/app.js
+++ www/assets/js/app.js
@@ -1,4 +1,8 @@
-var bN = (function($){
+var __CONF__ = {
+	speedThreshold: 5 //ms
+}
+
+var bN = (function($, config){
 	var _localSocket = 'http://localhost',
 		_BRKeoSocket = 'http://192.168.5.126',
 
@@ -6,7 +10,9 @@ var bN = (function($){
 
 	_port = "8080",
 
-	_io;
+	_io,
+
+	_messages = {};
 
 	return {
 		io: io.connect(_currentSocket),
@@ -22,17 +28,28 @@ var bN = (function($){
 				document.head.appendChild(script);
 				if(i == iLen - 1){
 					script.onload = function(){
-						IO.emit("clientReady");
+						bN.emit("clientReady");
 					}
 				}
 			}
 			
 		},
 		emit: function(message, data){
-			
+			_messages[message] = _messages[message] || {};
+
+			if(_messages[message].timeout) {
+				clearTimeout(_messages[message].timeout);
+				_messages[message].timeout = setTimeout(function(){
+					IO.emit(message, data);
+				}, config.speedThreshold);
+			} else {
+				_messages[message].timeout = setTimeout(function(){
+					IO.emit(message, data);
+				}, config.speedThreshold);
+			}
 		}
 	}
-}(jQuery));
+}(jQuery, __CONF__));
 
 $(document).ready(function(){
 	bN.init();
